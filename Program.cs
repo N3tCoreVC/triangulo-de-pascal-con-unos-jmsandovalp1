d59eb28e95cbb6cddb55cd093b6978be7e7de149
Miguel Sandoval <msandoval@aime.mx>
2018-09-21 17:02:25 -0500

    Subiendo archivos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace triangulo_de_pascal_con_unos_jmsandovalp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
